4c893dcf328433128d823044350e23fc21390c9c
Lolian <test.md522@gmail.com>
2020-06-28 16:35:28 +0300

    015/ eu1

--- script.js
+++ script.js
@@ -10,18 +10,36 @@ const personalMoviesDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних фильмов?', ''),
+//           b = prompt('На сколько оцените его', '');
+//
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMoviesDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+//     if (personalMoviesDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов");
+//     } else if (personalMoviesDB.count >=10 && personalMoviesDB.count < 30) {
+//         console.log("Вы класический зритель");
+//     } else if (personalMoviesDB.count >= 30) {
+//         console.log("Вы киноман");
+//     } else {
+//            console.log("Произошла ошибка");
+//     }
+// console.log(personalMoviesDB);
+
+let i = 0;
+while (i < 2) {
     const a = prompt('Один из последних фильмов?', ''),
           b = prompt('На сколько оцените его', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMoviesDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
+    personalMoviesDB.movies[a] = b;
+    i++;
+ }
     if (personalMoviesDB.count < 10) {
         console.log("Просмотрено довольно мало фильмов");
     } else if (personalMoviesDB.count >=10 && personalMoviesDB.count < 30) {
@@ -31,5 +49,8 @@ for (let i = 0; i < 2; i++) {
     } else {
            console.log("Произошла ошибка");
     }
+
 console.log(personalMoviesDB);
+
+
 
