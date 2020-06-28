f946a5596816cb8e5975024be4e34155aff24071
Lolian <test.md522@gmail.com>
2020-06-28 16:06:41 +0300

    Практика ч.2

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Привет Мир!');
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних фильмов?', ''),
+          b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+    if (personalMoviesDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMoviesDB.count >=10 && personalMoviesDB.count < 30) {
+        console.log("Вы класический зритель");
+    } else if (personalMoviesDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+           console.log("Произошла ошибка");
+    }
+console.log(personalMoviesDB);
+
